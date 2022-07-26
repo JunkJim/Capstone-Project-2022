399eb59bef06645965a0a4ab55e9209ce7b33d53
JunkJim <80695262+JunkJim@users.noreply.github.com>
2022-07-26 15:57:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function revealMessage() {
+    document.getElementById("hiddenMessage"). style.display ='block';
+}
+
+console.log("This is my first Website hope you enjoy navigating through it");
